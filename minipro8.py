7dedb605608378a6fd545bac96e294856ec86137
Kishan Sindhi <83533427+kishanSindhi@users.noreply.github.com>
2021-05-31 22:34:56 +0530

    Update minipro8.py

--- minipro8.py
+++ minipro8.py
@@ -1,3 +1,8 @@
+# Author - Kishan Sindhi
+# date - 30-5-2021
+# discription - this function take the year as input from the user
+# and in return it tells that the entered tear us a leap yaer or not
+
 year = int(input("Enter a year you want to check that is a leap year or not:\n"))
 if year % 4 == 0:
     print("Year is leap year")
