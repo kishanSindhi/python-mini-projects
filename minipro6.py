22454a53d4c62674c96cd04c530582c8e7859d96
Kishan Sindhi <83533427+kishanSindhi@users.noreply.github.com>
2021-05-31 22:29:07 +0530

    Update minipro6.py

--- minipro6.py
+++ minipro6.py
@@ -1,3 +1,9 @@
+# Author - Kishan Sindhi
+# date - 31-5-2021
+# discription - This is a simple function of email slicing 
+# here you can enter your email id and this function will return you your "username" & "domain"
+
+
 email = input("Enter your email id\n")
 username = email[:email.index("@")]
 domain  = email[email.index("@")+1:]
